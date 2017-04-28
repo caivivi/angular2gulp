6917f8ebd0fe12534410ac4972b62e9d1de29122
chris <sky93983@sina.com>
2017-04-28 17:17:18 +0800

    Remove useless config.

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -5,12 +5,10 @@ requirejs.config({
     baseUrl: "/",
     waitSeconds: 2,
     paths: {
-        "lodash": `${libFolder}lodash`,
-        "corejs": `${libFolder}shim`,
-        "bundle": `${libFolder}bundle`
+        
     }
 });
 
 requirejs(["@angular/platform-browser-dynamic", "modules/app/app.module"], ({ platformBrowserDynamic }, { AppModule }) => {
     platformBrowserDynamic().bootstrapModule(AppModule).catch((err) => console.error(`Application bootstraping error:`, err));
-}); 
+});
