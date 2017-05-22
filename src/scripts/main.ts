c9eaca395bfaca5a4520e7d23a9cda01dab68ec9
Song <ZCKASONG@zeiss.com>
2017-05-22 17:08:08 +0800

    update log message for service worker.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -12,7 +12,7 @@
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log('ServiceWorker registration successfully', registration);
     }).catch((err) => console.log('ServiceWorker registration failed: ', err));
 }
 
--- src/sw.ts
+++ src/sw.ts
@@ -17,11 +17,14 @@ self.addEventListener('fetch', (e: any) => {
         let response: Response = await caches.match(e.request);
 
         if (!response) {
+            console.log("Request doesn't exist from cache, fetching from server...", e.request);
             let fetchRequest = e.request.clone();
             response = await fetch(fetchRequest);
             let responseToCache = response.clone();
             let cache = await caches.open(appCache);
             cache.put(e.request, responseToCache);
+        } else {
+            console.log("Request pulled from cache", e.request);
         }
 
         return response;
