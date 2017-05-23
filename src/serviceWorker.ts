9b4accb23624426769410926a52e2ca7d777fb1d
Song <ZCKASONG@zeiss.com>
2017-05-23 11:11:25 +0800

    format code.

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -4,27 +4,23 @@ const appCache = "app_cache_v1", cacheFiles = [
     'scripts/main.js'
 ];
 
-self.addEventListener("install", (e: any) => {
-    e.waitUntil(async () => {
+self.addEventListener("install", (e: any) => e.waitUntil(async () => {
+    let cache = await caches.open(appCache);
+    await cache.addAll(cacheFiles);
+}));
+
+self.addEventListener('activate', (e: any) => e.waitUntil(async () => {
+
+}));
+
+this.addEventListener('fetch', (e) => e.respondWith((async () => {
+    let response = await caches.match(e.request);
+
+    if (!response) {
+        response = await fetch(e.request);
         let cache = await caches.open(appCache);
-        await cache.addAll(cacheFiles);
-    });
-});
+        cache.put(e.request, response.clone());
+    }
 
-self.addEventListener('activate', (e: any) => {
-    e.waitUntil(async () => { });
-});
-
-this.addEventListener('fetch', (e) => {
-    e.respondWith((async () => {
-        let response = await caches.match(e.request);
-
-        if (!response) {
-            response = await fetch(e.request);
-            let cache = await caches.open(appCache);
-            cache.put(e.request, response.clone());
-        }
-
-        return response;
-    })());
-});
+    return response;
+})()));
